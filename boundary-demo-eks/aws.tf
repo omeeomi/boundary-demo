b0e080a3accaa75dc369ab7600880abc8facf96a
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2023-04-18 13:19:11 -0700

    Update aws.tf

--- boundary-demo-eks/aws.tf
+++ boundary-demo-eks/aws.tf
@@ -221,7 +221,9 @@ resource "aws_db_instance" "postgres" {
   allocated_storage      = 10
   db_name                = "postgres"
   engine                 = "postgres"
-  engine_version         = "12.7"
+  engine_version         = "12.11"
+  allow_major_version_upgrade = false
+  auto_minor_version_upgrade = false
   instance_class         = "db.t3.micro"
   username               = var.db_user
   password               = var.db_password
