93ce55a3cc12e01d04e82e096091f3c1067530c0
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2023-04-18 20:34:56 -0700

    Update aws.tf

--- boundary-demo-eks/aws.tf
+++ boundary-demo-eks/aws.tf
@@ -153,7 +153,7 @@ module "eks" {
   }
 }
 
-# SSH Target - A
+# SSH Targets
 resource "aws_instance" "ssh-cert-target" {
   depends_on = [
     vault_ssh_secret_backend_ca.ssh_ca, aws_vpc_peering_connection_options.dns
@@ -163,7 +163,7 @@ resource "aws_instance" "ssh-cert-target" {
   }
   ami           = data.aws_ami.aws_linux_hvm2.id
   instance_type = "t3.micro"
-
+  count        = 3
   key_name                    = data.aws_key_pair.aws_key_name.key_name
   monitoring                  = true
   subnet_id                   = module.boundary-eks-vpc.private_subnets[1]
@@ -173,55 +173,7 @@ resource "aws_instance" "ssh-cert-target" {
 
   tags = {
     Team = "PIE"
-    Name = "ssh-cert-target"
-  }
-}
-
-# SSH Target - B
-resource "aws_instance" "ssh-cert-target-b" {
-  depends_on = [
-    vault_ssh_secret_backend_ca.ssh_ca, aws_vpc_peering_connection_options.dns
-  ]
-  lifecycle {
-    ignore_changes = [user_data_base64]
-  }
-  ami           = data.aws_ami.aws_linux_hvm2.id
-  instance_type = "t3.micro"
-
-  key_name                    = data.aws_key_pair.aws_key_name.key_name
-  monitoring                  = true
-  subnet_id                   = module.boundary-eks-vpc.private_subnets[1]
-  vpc_security_group_ids      = [module.ssh-cert-sec-group.security_group_id]
-  user_data_base64            = data.cloudinit_config.ssh_cert_target.rendered
-  user_data_replace_on_change = true
-
-  tags = {
-    Team = "PIE"
-    Name = "ssh-cert-target-b"
-  }
-}
-
-# SSH Target - C
-resource "aws_instance" "ssh-cert-target-c" {
-  depends_on = [
-    vault_ssh_secret_backend_ca.ssh_ca, aws_vpc_peering_connection_options.dns
-  ]
-  lifecycle {
-    ignore_changes = [user_data_base64]
-  }
-  ami           = data.aws_ami.aws_linux_hvm2.id
-  instance_type = "t3.micro"
-
-  key_name                    = data.aws_key_pair.aws_key_name.key_name
-  monitoring                  = true
-  subnet_id                   = module.boundary-eks-vpc.private_subnets[1]
-  vpc_security_group_ids      = [module.ssh-cert-sec-group.security_group_id]
-  user_data_base64            = data.cloudinit_config.ssh_cert_target.rendered
-  user_data_replace_on_change = true
-
-  tags = {
-    Team = "PIE"
-    Name = "ssh-cert-target-c"
+    Name = "ssh-cert-target-${count.index}"
   }
 }
     
